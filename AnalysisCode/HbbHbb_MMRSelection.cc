579b866c10b733dcfa74ca1914c8d036aef58299
Caterina <caterina.vernieri@cern.ch>
2015-08-21 10:18:48 -0500

    added purity

--- AnalysisCode/HbbHbb_MMRSelection.cc
+++ AnalysisCode/HbbHbb_MMRSelection.cc
@@ -46,9 +46,10 @@ void HbbHbb_MMRSelection(std::string sample)
   
   // Book variables
   double eventWeight;
-  int nJets;
+  int nJets, nGenBQuarkFromH;
   double jet_btagCSV[100], jet_btagCMVA[100];
   double jet_pT[100], jet_eta[100], jet_phi[100], jet_mass[100];
+  double genBQuarkFromH_pT[4],genBQuarkFromH_eta[4],genBQuarkFromH_phi[4],genBQuarkFromH_mass[4];
   std::vector<unsigned int> *jetIndex_pTOrder=0;
   
   // Retrieve variables
@@ -61,6 +62,12 @@ void HbbHbb_MMRSelection(std::string sample)
   tree->SetBranchAddress("Jet_phi", &(jet_phi));                  
   tree->SetBranchAddress("Jet_mass", &(jet_mass));                
   tree->SetBranchAddress("jetIndex_pTOrder", &(jetIndex_pTOrder));
+  tree->SetBranchAddress("nGenBQuarkFromH", &(nGenBQuarkFromH));         
+  tree->SetBranchAddress("GenBQuarkFromH_pt", &(genBQuarkFromH_pT));     
+  tree->SetBranchAddress("GenBQuarkFromH_eta", &(genBQuarkFromH_eta));   
+  tree->SetBranchAddress("GenBQuarkFromH_phi", &(genBQuarkFromH_phi));   
+  tree->SetBranchAddress("GenBQuarkFromH_mass", &(genBQuarkFromH_mass));
+
   
   // Book histograms
   TH1F *h_H1_mass=new TH1F("h_H1_mass", "H1 mass; mass (GeV)", 50, 50., 200.);
@@ -68,6 +75,9 @@ void HbbHbb_MMRSelection(std::string sample)
   TH1F *h_H2_mass=new TH1F("h_H2_mass", "H2 mass; mass (GeV)", 50, 50., 200.);
   TH1F *h_H2_pT=new TH1F("h_H2_pT", "H2 p_{T}; p_{T} (GeV/c)", 50, 0., 800.);
   TH1F *h_mX_SR=new TH1F("h_mX_SR", "h_mX_SR", 200, 0., 2000.); h_mX_SR->Sumw2();
+  TH1F *h_Xmass_right = new TH1F("h_Xmass_right"," h_Xmass right" , 100, 0., 1000.);
+  TH1F *h_Xmass_wrong = new TH1F("h_Xmass_wrong"," h_Xmass wrong" , 100, 0., 1000.);
+  
   
   // Get the h_Cuts histogram
   std::string histfilename="Histograms_"+sample+".root";
@@ -77,7 +87,7 @@ void HbbHbb_MMRSelection(std::string sample)
   tFile1->Close();
   
   // Event loop
-  double nCut4=0, nCut5=0;
+  double nCut4=0, nCut5=0, nCutGen=0;
   for (int i=0; i<tree->GetEntries(); ++i)
   {
     tree->GetEvent(i);
@@ -86,6 +96,13 @@ void HbbHbb_MMRSelection(std::string sample)
     double m_diff_old=50.;
     int H1jet1_i, H1jet2_i;
     int H2jet1_i, H2jet2_i;
+  
+    TLorentzVector hb[4];
+    for(int i=0; i<4; i++)
+    {	
+    hb[i].SetPtEtaPhiM(GenBQuarkFromH_pt[i], GenBQuarkFromH_eta[i], GenBQuarkFromH_phi[i], GenBQuarkFromH_mass[i]); 
+    }
+
     for (unsigned int j=0; j<jetIndex_pTOrder->size(); ++j)
     {
       unsigned int j_jetIndex=jetIndex_pTOrder->at(j);
@@ -124,58 +141,105 @@ void HbbHbb_MMRSelection(std::string sample)
                     H2jet1_i=l_jetIndex;
                     H2jet2_i=m_jetIndex;
                     m_diff_old=m_diff;
-                    foundHH=true;
-                  }
-                } // Conditions on 4th jet
-              } // Loop over 4th jet              
-            } // Conditions on 3rd jet        
-          } // Loop over 3rd jet
-        } // Conditions on 2nd jet
+		    foundHH=true;
+
+		    //check purity
+		    double dRbj[4][4];
+		    for (int ij =0; ij<4; ij++){
+			    for(int ji =0 ;ji<4; ji++){
+				    dRbj[ij][ji]=999.;
+			    }
+		    }
+		    match = false;
+		    for (int m =0; m<4; m++)
+		    {
+	
+		    dRbj[m][0] = hb[m].DeltaR(jet1_p4);
+		    dRbj[m][1] = hb[m].DeltaR(jet2_p4);
+		    dRbj[m][2] = hb[m].DeltaR(jet3_p4);
+		    dRbj[m][3] = hb[m].DeltaR(jet4_p4);
+		    }
+		    int n=0;	
+		    int ji_match[4] ={-1,-1,-1,-1};
+		    for (int ij =0; ij<4; ij++){
+                            for(int ji =0 ;ji<4; ji++){
+                                
+				   if(dRbj[ij][ji]<0.3 && ji_match[ji]<0){
+
+					   n++;
+					   ji_match[ji]=ji;
+					   
+
+				   }
+
+			    }
+		    }
+		    if(n>=4) match =true;	
+
+
+
+
+		  }
+		} // Conditions on 4th jet
+	      } // Loop over 4th jet              
+	    } // Conditions on 3rd jet        
+	  } // Loop over 3rd jet
+	} // Conditions on 2nd jet
       } // Loop over 2nd jet
     } // Loop over 1st jet
-    
+
     if (foundHH)
     {
-      nCut4+=eventWeight;
-      
-      TLorentzVector jet1_p4=fillTLorentzVector(jet_pT[H1jet1_i], jet_eta[H1jet1_i], jet_phi[H1jet1_i], jet_mass[H1jet1_i]);
-      TLorentzVector jet2_p4=fillTLorentzVector(jet_pT[H1jet2_i], jet_eta[H1jet2_i], jet_phi[H1jet2_i], jet_mass[H1jet2_i]);    
-      TLorentzVector jet3_p4=fillTLorentzVector(jet_pT[H2jet1_i], jet_eta[H2jet1_i], jet_phi[H2jet1_i], jet_mass[H2jet1_i]);    
-      TLorentzVector jet4_p4=fillTLorentzVector(jet_pT[H2jet2_i], jet_eta[H2jet2_i], jet_phi[H2jet2_i], jet_mass[H2jet2_i]);
-                  
-      // Randomization or ordering of which Higgs is which
-      if (int((jet1_p4+jet2_p4).Pt()*100.) % 2 == 1) {swap(H1jet1_i, H2jet1_i); swap(H1jet2_i, H2jet2_i);} // swap if H pT is odd in second decimal place
-      
-      jet1_p4=fillTLorentzVector(jet_pT[H1jet1_i], jet_eta[H1jet1_i], jet_phi[H1jet1_i], jet_mass[H1jet1_i]);
-      jet2_p4=fillTLorentzVector(jet_pT[H1jet2_i], jet_eta[H1jet2_i], jet_phi[H1jet2_i], jet_mass[H1jet2_i]); 
-      jet3_p4=fillTLorentzVector(jet_pT[H2jet1_i], jet_eta[H2jet1_i], jet_phi[H2jet1_i], jet_mass[H2jet1_i]); 
-      jet4_p4=fillTLorentzVector(jet_pT[H2jet2_i], jet_eta[H2jet2_i], jet_phi[H2jet2_i], jet_mass[H2jet2_i]);
-      
-      TLorentzVector H1_p4=jet1_p4+jet2_p4;
-      TLorentzVector H2_p4=jet3_p4+jet4_p4;
-      TLorentzVector X_p4=H1_p4+H2_p4;
-      
-      h_H1_mass->Fill(H1_p4.M(), eventWeight);
-      h_H1_pT->Fill(H1_p4.Pt(), eventWeight);
-      h_H2_mass->Fill(H2_p4.M(), eventWeight);
-      h_H2_pT->Fill(H2_p4.Pt(), eventWeight);
-            
-      int region=withinRegion(H1_p4.M(), H2_p4.M(), 17.5, 35., H_mass, H_mass);
-      
-      if (region==0) // SR
-      {
-        nCut5+=eventWeight;
-        
-        h_mX_SR->Fill(X_p4.M(), eventWeight);
-      }
-      
+	    nCut4+=eventWeight;
+	    if(match) nCutGen++;
+
+	    TLorentzVector jet1_p4=fillTLorentzVector(jet_pT[H1jet1_i], jet_eta[H1jet1_i], jet_phi[H1jet1_i], jet_mass[H1jet1_i]);
+	    TLorentzVector jet2_p4=fillTLorentzVector(jet_pT[H1jet2_i], jet_eta[H1jet2_i], jet_phi[H1jet2_i], jet_mass[H1jet2_i]);    
+	    TLorentzVector jet3_p4=fillTLorentzVector(jet_pT[H2jet1_i], jet_eta[H2jet1_i], jet_phi[H2jet1_i], jet_mass[H2jet1_i]);    
+	    TLorentzVector jet4_p4=fillTLorentzVector(jet_pT[H2jet2_i], jet_eta[H2jet2_i], jet_phi[H2jet2_i], jet_mass[H2jet2_i]);
+
+	    // Randomization or ordering of which Higgs is which
+	    if (int((jet1_p4+jet2_p4).Pt()*100.) % 2 == 1) {swap(H1jet1_i, H2jet1_i); swap(H1jet2_i, H2jet2_i);} // swap if H pT is odd in second decimal place
+
+	    jet1_p4=fillTLorentzVector(jet_pT[H1jet1_i], jet_eta[H1jet1_i], jet_phi[H1jet1_i], jet_mass[H1jet1_i]);
+	    jet2_p4=fillTLorentzVector(jet_pT[H1jet2_i], jet_eta[H1jet2_i], jet_phi[H1jet2_i], jet_mass[H1jet2_i]); 
+	    jet3_p4=fillTLorentzVector(jet_pT[H2jet1_i], jet_eta[H2jet1_i], jet_phi[H2jet1_i], jet_mass[H2jet1_i]); 
+	    jet4_p4=fillTLorentzVector(jet_pT[H2jet2_i], jet_eta[H2jet2_i], jet_phi[H2jet2_i], jet_mass[H2jet2_i]);
+
+	    TLorentzVector H1_p4=jet1_p4+jet2_p4;
+	    TLorentzVector H2_p4=jet3_p4+jet4_p4;
+	    TLorentzVector X_p4=H1_p4+H2_p4;
+
+	    h_H1_mass->Fill(H1_p4.M(), eventWeight);
+	    h_H1_pT->Fill(H1_p4.Pt(), eventWeight);
+	    h_H2_mass->Fill(H2_p4.M(), eventWeight);
+	    h_H2_pT->Fill(H2_p4.Pt(), eventWeight);
+	    if(match)  {
+		    h_Xmass_right->Fill((H1_p4+H2_p4).M(), eventWeight);
+		    matched=1;
+	    }
+	    else {
+		    h_Xmass_wrong->Fill((H1_p4+H2_p4).M(), eventWeight);
+		    matched=0;
+	    }
+
+
+	    int region=withinRegion(H1_p4.M(), H2_p4.M(), 17.5, 35., H_mass, H_mass);
+
+	    if (region==0) // SR
+	    {
+		    nCut5+=eventWeight;
+
+		    h_mX_SR->Fill(X_p4.M(), eventWeight);
+	    }
+
     }
-    
+
   } // Event loop
-    
+
   h_Cuts.Fill(9, nCut4); // HH Candidates
   h_Cuts.Fill(11, nCut5); // SR
-    
+
   TFile *tFile2=new TFile(histfilename.c_str(), "UPDATE");
   tFile2->Delete("h_Cuts;1");
   h_H1_mass->Write();
@@ -183,15 +247,19 @@ void HbbHbb_MMRSelection(std::string sample)
   h_H2_mass->Write();
   h_H2_pT->Write();
   h_mX_SR->Write();
+  h_Xmass_right->Write();
+  h_Xmass_wrong->Write();
   h_Cuts.Write();
-  
+
   tFile2->Write();
   tFile2->Close();
   std::cout<<"Wrote output file "<<histfilename<<std::endl;
-  
+
   std::cout<<"=== Cut Efficiencies === "<<std::endl;
   std::cout<<"Number of events after finding HH candidate (btag && pT>40 GeV && |eta|<2.5)  = "<<nCut4<<std::endl;
+  std::cout<<"Number of matched events "<<(float)nCutGen/nCut4<<std::endl;
+
   std::cout<<"Number of events in SR = "<<nCut5<<std::endl;
   std::cout<<"========================"<<std::endl;
-  
+
 }
