e84ed32393176ec03712d088579f3587c29b2c18
souvik1982 <souvik1982@gmail.com>
2015-11-21 03:42:14 -0600

    jet properties

--- AnalysisCode/DisplayJetProperties.cc
+++ AnalysisCode/DisplayJetProperties.cc
@@ -46,6 +46,7 @@ void DisplayJetProperties()
   v_files.push_back(new TFile("Histograms_Graviton800GeV.root"));
   v_files.push_back(new TFile("Histograms_Graviton1000GeV.root"));
   TFile *f_data=new TFile("Histograms_Data_BTagCSV_2015_Skim.root");
+  TFile *f_ttbar=new TFile("Histograms_TT_TuneCUETP8M1_13TeV-amcatnlo-pythia8_Skim.root");
   std::vector <double> mean_gen={300, 400, 600, 800, 1000};
   std::vector<int> v_colors = {kGreen, kGreen+2, kOrange, kOrange+2, kMagenta, kMagenta+2, kBlue, kBlue+2, kCyan, kCyan+2};
   
@@ -61,6 +62,7 @@ void DisplayJetProperties()
     leg->AddEntry(v_files.at(i)->Get("h_pTOrder_JetpT_1"), ("Signal m_{X} = "+itoa(mean_gen.at(i))+" GeV").c_str());
   }
   leg->AddEntry(f_data->Get("h_pTOrder_JetpT_1"), "13 TeV Data");
+  leg->AddEntry(f_ttbar->Get("h_pTOrder_JetpT_1"), "t#bar{t}");
   
   // Plot the 1st highest pT jets with |eta| < 2.5  
   first=true;
@@ -70,6 +72,7 @@ void DisplayJetProperties()
     DisplayHistogram_forFile(v_files.at(i), "h_pTOrder_JetpT_1", v_colors.at(i));
   }
   DisplayHistogram_forFile(f_data, "h_pTOrder_JetpT_1", kBlack);
+  DisplayHistogram_forFile(f_ttbar, "h_pTOrder_JetpT_1", kRed);
   leg->Draw();
   TArrow *line=new TArrow(40., 0.14, 40., 0); line->SetLineWidth(2);
   line->Draw();
@@ -83,6 +86,7 @@ void DisplayJetProperties()
     DisplayHistogram_forFile(v_files.at(i), "h_pTOrder_JetpT_2", v_colors.at(i));
   }
   DisplayHistogram_forFile(f_data, "h_pTOrder_JetpT_2", kBlack);
+  DisplayHistogram_forFile(f_ttbar, "h_pTOrder_JetpT_2", kRed);
   leg->Draw();
   line->Draw();
   c_pTOrder_JetpT_2->SaveAs("c_pTOrder_JetpT_2.png");
@@ -95,6 +99,7 @@ void DisplayJetProperties()
     DisplayHistogram_forFile(v_files.at(i), "h_pTOrder_JetpT_3", v_colors.at(i));
   }
   DisplayHistogram_forFile(f_data, "h_pTOrder_JetpT_3", kBlack);
+  DisplayHistogram_forFile(f_ttbar, "h_pTOrder_JetpT_3", kRed);
   leg->Draw();
   line->Draw();
   c_pTOrder_JetpT_3->SaveAs("c_pTOrder_JetpT_3.png");
@@ -107,6 +112,7 @@ void DisplayJetProperties()
     DisplayHistogram_forFile(v_files.at(i), "h_pTOrder_JetpT_4", v_colors.at(i));
   }
   DisplayHistogram_forFile(f_data, "h_pTOrder_JetpT_4", kBlack);
+  DisplayHistogram_forFile(f_ttbar, "h_pTOrder_JetpT_4", kRed);
   leg->Draw();
   line->Draw();
   c_pTOrder_JetpT_4->SaveAs("c_pTOrder_JetpT_4.png");
@@ -119,6 +125,7 @@ void DisplayJetProperties()
     DisplayHistogram_forFile(v_files.at(i), "h_pTOrder_JetpT_5", v_colors.at(i));
   }
   DisplayHistogram_forFile(f_data, "h_pTOrder_JetpT_5", kBlack);
+  DisplayHistogram_forFile(f_ttbar, "h_pTOrder_JetpT_5", kRed);
   leg->Draw();
   c_pTOrder_JetpT_5->SaveAs("c_pTOrder_JetpT_5.png");
   
@@ -131,8 +138,9 @@ void DisplayJetProperties()
     DisplayHistogram_forFile(v_files.at(i), "h_CSVOrder_JetCSV_1", v_colors.at(i));
   }
   DisplayHistogram_forFile(f_data, "h_CSVOrder_JetCSV_1", kBlack);
+  DisplayHistogram_forFile(f_ttbar, "h_CSVOrder_JetCSV_1", kRed);
   leg->Draw();
-  line=new TArrow(0.898, 0.14, 0.898, 0); line->SetLineWidth(2);
+  line=new TArrow(0.898, 0.8, 0.898, 0); line->SetLineWidth(2);
   line->Draw();
   c_CSVOrder_JetCSV_1->SaveAs("c_CSVOrder_JetCSV_1.png");
   
@@ -144,8 +152,9 @@ void DisplayJetProperties()
     DisplayHistogram_forFile(v_files.at(i), "h_CSVOrder_JetCSV_2", v_colors.at(i));
   }
   DisplayHistogram_forFile(f_data, "h_CSVOrder_JetCSV_2", kBlack);
+  DisplayHistogram_forFile(f_ttbar, "h_CSVOrder_JetCSV_2", kRed);
   leg->Draw();
-  line=new TArrow(0.898, 0.14, 0.898, 0); line->SetLineWidth(2);
+  line=new TArrow(0.898, 0.4, 0.898, 0); line->SetLineWidth(2);
   line->Draw();
   c_CSVOrder_JetCSV_2->SaveAs("c_CSVOrder_JetCSV_2.png");
   
@@ -157,21 +166,23 @@ void DisplayJetProperties()
     DisplayHistogram_forFile(v_files.at(i), "h_CSVOrder_JetCSV_3", v_colors.at(i));
   }
   DisplayHistogram_forFile(f_data, "h_CSVOrder_JetCSV_3", kBlack);
+  DisplayHistogram_forFile(f_ttbar, "h_CSVOrder_JetCSV_3", kRed);
   leg->Draw();
-  line=new TArrow(0.898, 0.14, 0.898, 0); line->SetLineWidth(2);
+  line=new TArrow(0.898, 0.1, 0.898, 0); line->SetLineWidth(2);
   line->Draw();
   c_CSVOrder_JetCSV_3->SaveAs("c_CSVOrder_JetCSV_3.png");
   
   // Plot the 4th highest CSV jets with |eta| < 2.5, pT > 40 GeV  
   first=true;
   TCanvas *c_CSVOrder_JetCSV_4=new TCanvas("c_CSVOrder_JetCSV_4", "c_CSVOrder_JetCSV_4", 700, 700);
+  DisplayHistogram_forFile(f_data, "h_CSVOrder_JetCSV_4", kBlack);
   for (unsigned int i=0; i<v_files.size(); ++i)
   {
     DisplayHistogram_forFile(v_files.at(i), "h_CSVOrder_JetCSV_4", v_colors.at(i));
   }
-  DisplayHistogram_forFile(f_data, "h_CSVOrder_JetCSV_4", kBlack);
+  DisplayHistogram_forFile(f_ttbar, "h_CSVOrder_JetCSV_4", kRed);
   leg->Draw();
-  line=new TArrow(0.898, 0.14, 0.898, 0); line->SetLineWidth(2);
+  line=new TArrow(0.898, 0.05, 0.898, 0); line->SetLineWidth(2);
   line->Draw();
   c_CSVOrder_JetCSV_4->SaveAs("c_CSVOrder_JetCSV_4.png");
   
