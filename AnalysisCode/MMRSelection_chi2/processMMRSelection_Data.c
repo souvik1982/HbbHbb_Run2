5fa94bbb346d3015a9614e0324b1d9113d912df7
Souvik <souvik1982@gmail.com>
2015-11-24 03:17:52 -0600

    Adding chi^2 selection

---
+++ AnalysisCode/MMRSelection_chi2/processMMRSelection_Data.c
@@ -0,0 +1,6 @@
+{
+  gSystem->Load("../libPhysicsToolsKinFitter.so");
+  gROOT->LoadMacro("../HbbHbb_MMRSelection_chi2.cc++");
+  HbbHbb_MMRSelection_chi2("Data", "Data_BTagCSV_2015C_Skim");
+  HbbHbb_MMRSelection_chi2("Data", "Data_BTagCSV_2015D_Skim");
+}
