cc4d3b621e459ca69a615e5cd2345509ba629f4b
Souvik <souvik1982@gmail.com>
2015-12-27 04:45:13 -0600

    Regression

--- AnalysisCode/MMRSelection/processMMRSelection_ttbar.c
+++
@@ -1,5 +0,0 @@
-{
-  gSystem->Load("../libPhysicsToolsKinFitter.so");
-  gROOT->LoadMacro("../HbbHbb_MMRSelection.cc++");
-  HbbHbb_MMRSelection("Data", "TT_TuneCUETP8M1_13TeV-amcatnlo-pythia8_Skim");
-}
