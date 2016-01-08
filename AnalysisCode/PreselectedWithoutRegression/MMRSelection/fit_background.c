5cdf92792d4b80aa0445ca97cb78b7c18fe4b22f
Souvik <souvik1982@gmail.com>
2016-01-08 16:07:19 -0600

    background fitting script

--- AnalysisCode/PreselectedWithoutRegression/MMRSelection/fit_background.c
+++ AnalysisCode/PreselectedWithoutRegression/MMRSelection/fit_background.c
@@ -1,5 +1,5 @@
 {
   gSystem->Load("../../PDFs/GaussExp_cxx.so");
   gROOT->LoadMacro("../../BackgroundPrediction_Kinematic_GaussExp.c");
-  BackgroundPrediction_Kinematic_GaussExp(150, 2650, 250, 2550);
+  BackgroundPrediction_Kinematic_GaussExp(150, 2650, 250, 2550, "log");
 }
