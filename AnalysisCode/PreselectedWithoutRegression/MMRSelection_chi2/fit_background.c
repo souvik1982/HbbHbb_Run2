85516cc946c5474b05e766c76f5e4749e3be65aa
Souvik <souvik1982@gmail.com>
2016-01-16 04:28:38 -0600

    working datacards

--- AnalysisCode/PreselectedWithoutRegression/MMRSelection_chi2/fit_background.c
+++ AnalysisCode/PreselectedWithoutRegression/MMRSelection_chi2/fit_background.c
@@ -1,8 +1,8 @@
 {
   gSystem->Load("../../PDFs/GaussExp_cxx.so");
   gROOT->LoadMacro("../../BackgroundPrediction_Kinematic_GaussExp.cc");
-  BackgroundPrediction_Kinematic_GaussExp(150, 2650, // plot range
-                                          250, 2550, // fit range
+  BackgroundPrediction_Kinematic_GaussExp(150, 2050, // plot range
+                                          250, 1850, // fit range
                                           "h_mX_SB_kinFit", // histogram
                                           "lin");
 }
