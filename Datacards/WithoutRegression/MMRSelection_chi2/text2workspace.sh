815770e7375c89d8fde3f5fd2f38147c66719f94
Souvik <souvik1982@gmail.com>
2016-01-16 04:26:49 -0600

    working datacards

--- Datacards/WithoutRegression/MMRSelection_chi2/text2workspace.sh
+++ Datacards/WithoutRegression/MMRSelection_chi2/text2workspace.sh
@@ -1,1 +1,6 @@
-text2workspace.py HbbHbb_13TeV_mX400.txt -o HbbHbb_13TeV_mX400.root -L ../../../AnalysisCode/PDFs/GaussExp_cxx.so -L ../../../AnalysisCode/PDFs/ExpGaussExp_cxx.so
+setenv dir /uscms_data/d2/souvik/HbbHbb_Run2/HbbHbb_Run2/AnalysisCode/PDFs
+text2workspace.py HbbHbb_13TeV_mX400.txt  -o HbbHbb_13TeV_mX400.root -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so
+text2workspace.py HbbHbb_13TeV_mX600.txt  -o HbbHbb_13TeV_mX600.root -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so
+text2workspace.py HbbHbb_13TeV_mX800.txt  -o HbbHbb_13TeV_mX800.root -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so
+text2workspace.py HbbHbb_13TeV_mX1000.txt -o HbbHbb_13TeV_mX1000.root -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so
+text2workspace.py HbbHbb_13TeV_mX1200.txt -o HbbHbb_13TeV_mX1200.root -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so
