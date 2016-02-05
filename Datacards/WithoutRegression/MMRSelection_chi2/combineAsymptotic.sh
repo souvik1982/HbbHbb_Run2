89f76a3029d6cf2919d79aaa8748f038e2e9f259
Caterina <caterina.vernieri@cern.ch>
2016-02-04 19:18:25 -0600

    datacards MMR

--- Datacards/WithoutRegression/MMRSelection_chi2/combineAsymptotic.sh
+++ Datacards/WithoutRegression/MMRSelection_chi2/combineAsymptotic.sh
@@ -1,11 +1,11 @@
-setenv dir ../../../AnalysisCode/PDFs
-combine -M Asymptotic HbbHbb_13TeV_mX400.root --noFitAsimov -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so >&! HbbHbb_13TeV_mX400_Asymptotic.log 
+combine -M Asymptotic HbbHbb_13TeV_mX400.root --noFitAsimov >&! HbbHbb_13TeV_mX400_Asymptotic.log
 echo mX400 done
-combine -M Asymptotic HbbHbb_13TeV_mX600.root --noFitAsimov -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so >&! HbbHbb_13TeV_mX600_Asymptotic.log
+combine -M Asymptotic HbbHbb_13TeV_mX600.root --noFitAsimov >&! HbbHbb_13TeV_mX600_Asymptotic.log
 echo mX600 done
-combine -M Asymptotic HbbHbb_13TeV_mX800.root --noFitAsimov -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so >&! HbbHbb_13TeV_mX800_Asymptotic.log
+combine -M Asymptotic HbbHbb_13TeV_mX800.root --noFitAsimov >&! HbbHbb_13TeV_mX800_Asymptotic.log
 echo mX800 done
-combine -M Asymptotic HbbHbb_13TeV_mX1000.root --noFitAsimov -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so >&! HbbHbb_13TeV_mX1000_Asymptotic.log
+combine -M Asymptotic HbbHbb_13TeV_mX1000.root --noFitAsimov >&! HbbHbb_13TeV_mX1000_Asymptotic.log
 echo mX1000 done
-combine -M Asymptotic HbbHbb_13TeV_mX1200.root --noFitAsimov -L $dir/GaussExp_cxx.so -L $dir/ExpGaussExp_cxx.so >&! HbbHbb_13TeV_mX1200_Asymptotic.log
+combine -M Asymptotic HbbHbb_13TeV_mX1200.root --noFitAsimov >&! HbbHbb_13TeV_mX1200_Asymptotic.log
 echo mX1200 done
+
