44d3caa2c960e000ebbc36b81b59d2a04ca2ff58
Caterina <caterina.vernieri@cern.ch>
2016-07-27 14:23:47 -0500

    2016 data

--- AnalysisCode/PreselectedWithRegression/processPreSelection_Data.sh
+++ AnalysisCode/PreselectedWithRegression/processPreSelection_Data.sh
@@ -1,11 +1,8 @@
-#setenv dir /eos/uscms/store/user/souvik/HbbHbb_Run2/V15
+setenv dir /eos/uscms/store/user/lpchbb/HeppyNtuples/V22reHLT/
 
-#root -l -b -q 'HbbHbb_PreSelection.cc++("$dir", "Data_BTagCSV_2015C_Skim", "TMVARegression_BDTG.weights.xml")'
-#root -l -b -q 'HbbHbb_PreSelection.cc++("$dir", "Data_BTagCSV_2015D_Skim", "TMVARegression_BDTG.weights.xml")'
-
-#hadd -f PreSelected_Data_BTagCSV_2015_Skim.root PreSelected_Data_BTagCSV_2015C_Skim.root PreSelected_Data_BTagCSV_2015D_Skim.root
-#hadd -f Histograms_Data_BTagCSV_2015_Skim.root Histograms_Data_BTagCSV_2015D_Skim.root Histograms_Data_BTagCSV_2015D_Skim.root 
-
-setenv dir /eos/uscms/store/user/cvernier/
-
-root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV_2015_Skim", "TMVARegression_BDTG.weights.xml")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV0","ttbar-fall15-TargetGenOverPt.xml")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV1","ttbar-fall15-TargetGenOverPt.xml")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV2","ttbar-fall15-TargetGenOverPt.xml")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV3","ttbar-fall15-TargetGenOverPt.xml")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV4","ttbar-fall15-TargetGenOverPt.xml")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV5","ttbar-fall15-TargetGenOverPt.xml")'
--- AnalysisCode/PreselectedWithoutRegression/processPreSelection_Data.sh
+++ AnalysisCode/PreselectedWithoutRegression/processPreSelection_Data.sh
@@ -1,3 +1,8 @@
-setenv dir /eos/uscms/store/user/lpchbb/HeppyNtuples/H21b/
+setenv dir /eos/uscms/store/user/lpchbb/HeppyNtuples/V22reHLT/
 
-root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV_Skim")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV0")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV1")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV2")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV3")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV4")'
+root -l -b -q '../HbbHbb_PreSelection.cc++("$dir", "BTagCSV5")'
