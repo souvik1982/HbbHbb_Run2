ed28c7bb50ebc0803f54f03e2f65b1455835962f
Caterina <caterina.vernieri@cern.ch>
2016-07-29 16:21:17 -0500

    test code

--- AnalysisCode/Trigger/Quadplots_fit_test_singleBtag.C
+++ AnalysisCode/Trigger/Quadplots_fit_test_singleBtag.C
@@ -1,4 +1,4 @@
-#include"fittedFunctions.h"
+//#include "fittedFunctions.h"
 //#include"fittedFunctions.h.GoodOld"
 
 void Quadplots_fit_test_singleBtag(){
@@ -10,30 +10,30 @@ void Quadplots_fit_test_singleBtag(){
     c1->SetGridy();
 
 //    TFile *_file0 = TFile::Open("SingleElectronVBF.root");
-    TFile *_file0 = TFile::Open("/eos/uscms/store/user/cvernier/SingleElectronVBF.root");
+    TFile *_file0 = TFile::Open("ZvvHighPt_V20_SingleMuon.root");
     TTree* tree = (TTree*) _file0->Get("tree");
 
     //////////////////////////////////////////////////
-    // plots for the single b-tag trigger, starting from (HLT_BIT_HLT_Ele23_WPLoose_Gsf_v && Alt$(Jet_pt[3],0)>30)
+    // plots for the single b-tag trigger, starting from (HLT_BIT_HLT_IsoMu18_v && Alt$(Jet_pt[3],0)>30)
     //////////////////////////////////////////////////
 
-    tree->Draw("Jet_pt[3]  >> h(25,0,200)"," QuadTriggerWeight(Jet_pt[0], Jet_pt[1], Jet_pt[2], Jet_pt[3], min(CSV[2],1-1.e-7))*(HLT_BIT_HLT_Ele23_WPLoose_Gsf_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4)","");
+    tree->Draw("Jet_pt[3]  >> h(25,0,200)"," TurnOnQuad(Jet_pt[0], Jet_pt[1], Jet_pt[2], Jet_pt[3], min(Jet_btagCSV[aJCidx[0]],1-1.e-7))*(HLT_BIT_HLT_IsoMu18_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4)","");
     h = (TProfile*) gDirectory->Get("h");h->SetLineColor(kBlack);h->SetMaximum(2.0*h->GetMaximum());
-    tree->Draw("Jet_pt[3]"," 1.0001*(HLT_BIT_HLT_QuadJet45_TripleBTagCSV0p67_v && (HLT_BIT_HLT_Ele23_WPLoose_Gsf_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4))","same");
+    tree->Draw("Jet_pt[3]"," 1.0001*(HLT_BIT_HLT_QuadJet45_TripleBTagCSV0p67_v && (HLT_BIT_HLT_IsoMu18_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4))","same");
 
     c1->SaveAs("check_full_pt4_single.png");
     c1->SaveAs("check_full_pt4_single.C");
 
-    tree->Draw("CSV[2]  >> h(25,0,1)"," QuadTriggerWeight(Jet_pt[0], Jet_pt[1], Jet_pt[2], Jet_pt[3], min(CSV[2],1-1.e-7))*(HLT_BIT_HLT_Ele23_WPLoose_Gsf_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4)","");
+    tree->Draw("Jet_btagCSV[aJCidx[0]]  >> h(25,0,1)"," TurnOnQuad(Jet_pt[0], Jet_pt[1], Jet_pt[2], Jet_pt[3], min(Jet_btagCSV[aJCidx[0]],1-1.e-7))*(HLT_BIT_HLT_IsoMu18_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4)","");
     h = (TProfile*) gDirectory->Get("h");h->SetLineColor(kBlack);h->SetMaximum(2.0*h->GetMaximum());
-    tree->Draw("CSV[2]"," 1.0001*(HLT_BIT_HLT_QuadJet45_TripleBTagCSV0p67_v && (HLT_BIT_HLT_Ele23_WPLoose_Gsf_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4))","same");
+    tree->Draw("Jet_btagCSV[aJCidx[0]]"," 1.0001*(HLT_BIT_HLT_QuadJet45_TripleBTagCSV0p67_v && (HLT_BIT_HLT_IsoMu18_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4))","same");
 
     c1->SaveAs("check_full_CSV3_single.png");
     c1->SaveAs("check_full_CSV3_single.C");
 
-    tree->Draw("Jet_eta[0]  >> h(25,-4,+4)"," QuadTriggerWeight(Jet_pt[0], Jet_pt[1], Jet_pt[2], Jet_pt[3], min(CSV[2],1-1.e-7))*(HLT_BIT_HLT_Ele23_WPLoose_Gsf_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4)","");
+    tree->Draw("Jet_eta[0]  >> h(25,-4,+4)"," TurnOnQuad(Jet_pt[0], Jet_pt[1], Jet_pt[2], Jet_pt[3], min(Jet_btagCSV[aJCidx[0]],1-1.e-7))*(HLT_BIT_HLT_IsoMu18_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4)","");
     h = (TProfile*) gDirectory->Get("h");h->SetLineColor(kBlack);h->SetMaximum(2.0*h->GetMaximum());
-    tree->Draw("Jet_eta[0]"," 1.0001*(HLT_BIT_HLT_QuadJet45_TripleBTagCSV0p67_v && (HLT_BIT_HLT_Ele23_WPLoose_Gsf_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4))","same");
+    tree->Draw("Jet_eta[0]"," 1.0001*(HLT_BIT_HLT_QuadJet45_TripleBTagCSV0p67_v && (HLT_BIT_HLT_IsoMu18_v && Alt$(Jet_pt[3],0)>30 && abs(Jet_eta[0])<2.4 && abs(Jet_eta[1])<2.4 && abs(Jet_eta[2])<2.4 && abs(Jet_eta[3])<2.4))","same");
 
     c1->SaveAs("check_full_eta0_single.png");
     c1->SaveAs("check_full_eta0_single.C");
