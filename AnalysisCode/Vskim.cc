6044236143b5feab71e09279702954a4a636eae2
Andrea Malara <andrea.malara@cern.ch>
2017-02-16 16:18:35 +0100

    Iterative skim

--- AnalysisCode/Vskim.cc
+++ AnalysisCode/Vskim.cc
@@ -9,16 +9,26 @@
 
 void Vskim(std::string source_dir, std::string dest_dir, std::string infile, int from, int to)
 {
-    std::string Dest_dir=gSystem->ExpandPathName("$dest_dir");
-    std::string Source_dir=gSystem->ExpandPathName("$source_dir");
-    std::string old_file_test=Dest_dir+"/tree_total_from_"+to_string(from)+"_to_"+to_string(to)+".root";
-    if(!gSystem->AccessPathName((old_file_test).c_str(),kFileExists)){ //Returns true if not found
-        gSystem->Exec(("mv "+Dest_dir+"/tree_total_from_"+to_string(from)+"_to_"+to_string(to)+".root"+" "+Dest_dir+"/tree_total_from_"+to_string(from)+"_to_"+to_string(to)+"_OLD.root").c_str());
-        std::cout<<"\033[1;31m mv "+Dest_dir+"/tree_total_from_"+to_string(from)+"_to_"+to_string(to)+".root"+" "+Dest_dir+"/tree_total_from_"+to_string(from)+"_to_"+to_string(to)+"_OLD.root"+"\033[0m"<< endl;
-    }
     bool flag = false;
+    int a = from;
+    int b = to;
+    
+    std::string source=source_dir.substr(12, 20);
+    std::string Source_dir=gSystem->ExpandPathName((source_dir).c_str());
+    std::string Dest_dir=gSystem->ExpandPathName((dest_dir).c_str());
+    std::string FinalFileName=Dest_dir+"/tree_"+source+"_from_"+to_string(from)+"_to_"+to_string(to)+".root";
+    std::string old_file_test=Dest_dir+"/tree_"+source+"_from_"+to_string(from)+"_to_"+to_string(to)+"_OLD.root";
+    
+    // Avoid overwrite files
+    if(!gSystem->AccessPathName((FinalFileName).c_str(),kFileExists)){ //Returns true if not found
+        gSystem->Exec(("mv "+FinalFileName+" "+old_file_test).c_str());
+        std::cout<<"\033[1;31m mv "+FinalFileName+" "+old_file_test+"\033[0m"<< endl;
+    }
+    
+    // Files loop
     for (int i = from; i<=to; i++) {
         std::string InputFileName=Source_dir+"/"+infile+to_string(i)+".root";
+        // Check if file exist
         if(!gSystem->AccessPathName((InputFileName).c_str(),kFileExists)){
             TFile *file=new TFile((InputFileName).c_str());
             TH1F *h_Count=(TH1F*)file->Get("Count");
@@ -27,8 +37,11 @@ void Vskim(std::string source_dir, std::string dest_dir, std::string infile, int
             TH1F *h_CountNegWeight=(TH1F*)file->Get("CountNegWeight");
             TTree *tree=(TTree*)file->Get("tree");
             
+            // Skim
             std::string cut="(Vtype == -1 || Vtype==4) && HLT_HH4bAll == 1";
             std::cout<<"Copying new " << i << " tree with "<<cut<<"... ";
+            
+            // Output tree in a new file
             std::string OutputFileName=Dest_dir+"/"+infile+to_string(i)+"_Skim.root";
             TFile *outfile=new TFile((OutputFileName).c_str(), "recreate");
             outfile->cd();
@@ -42,50 +55,44 @@ void Vskim(std::string source_dir, std::string dest_dir, std::string infile, int
             h_CountNegWeight->Write();
             outfile->Close();
             
-            std::string TempFileName=Dest_dir+"/temp.root";
-            std::string FinalFileName=Dest_dir+"/tree_total.root";
-            
-            if (i!=from || !gSystem->AccessPathName((FinalFileName).c_str(),kFileExists)){ //Returns true if not found
-                gSystem->Exec(("cp "+FinalFileName+" "+TempFileName).c_str());
-                //std::cout<<"cp "+FinalFileName+" "+TempFileName<< endl;
-                gSystem->Exec(("rm -fr "+FinalFileName).c_str());
-                //std::cout<<"rm -fr "+FinalFileName<< endl;
-                gSystem->Exec(("hadd "+FinalFileName+" "+TempFileName+" "+OutputFileName).c_str());
-                gSystem->Exec(("rm -fr "+OutputFileName).c_str());
-                //std::cout<<"rm -fr "+OutputFileName<< endl;
-                
-            }
-            else{
-                gSystem->Exec(("mv "+OutputFileName+" "+FinalFileName).c_str());
-                std::cout<<"mv "+OutputFileName+" "+FinalFileName<< endl;
+            // HADD
+            if (i%50==0) {
+                b = i;
+                std::string TempFileName=Dest_dir+"/temp_"+to_string(a)+"_"+to_string(b)+".root";
+                gSystem->Exec(("hadd "+TempFileName+" "+Dest_dir+"/"+infile+"*_Skim.root").c_str());
+		gSystem->Exec(("rm -fr "+Dest_dir+"/"+infile+"*_Skim.root").c_str());
+                a=i;
             }
         }
         else{
+            // Save not found files
             flag = true;
-            if (gSystem->AccessPathName((Dest_dir+"/problem.txt").c_str(),kFileExists)) {
+            if (gSystem->AccessPathName((Dest_dir+"/problem_"+source+".txt").c_str(),kFileExists)) {
                 std::ofstream myfile;
-                myfile.open((Dest_dir+"/problem.txt").c_str());
+                myfile.open((Dest_dir+"/problem_"+source+".txt").c_str());
                 myfile << "File" << "    "  << " Problem" << "    " << "File" <<"  "<< endl;
                 myfile << endl;
                 myfile.close();
             }
             
             std::ofstream myfile;
-            myfile.open((Dest_dir+"/problem.txt").c_str(), ios::app);
-            std:: string err_file = "tree_total_from_"+to_string(from)+"_to_"+to_string(to)+".root";
+            myfile.open((Dest_dir+"/problem_"+source+".txt").c_str(), ios::app);
+            std:: string err_file = "tree_"+source+"_from_"+to_string(from)+"_to_"+to_string(to)+".root";
             myfile << err_file << "    "  << "Not found file " << "    " << to_string(i) <<"  "<< endl;
             myfile.close();
         }
     }
-    gSystem->Exec(("mv "+Dest_dir+"/tree_total.root"+" "+Dest_dir+"/tree_total_from_"+to_string(from)+"_to_"+to_string(to)+".root").c_str());
-    gSystem->Exec(("rm -fr "+Dest_dir+"/temp.root").c_str());
-    std::cout<<"rm -fr "+Dest_dir+"/temp.root"<< endl;
+    // HADD of all files
+    gSystem->Exec(("hadd "+FinalFileName+" "+Dest_dir+"/temp_*.root").c_str());
+    gSystem->Exec(("rm -fr "+Dest_dir+"/temp_*.root").c_str());
+    gSystem->Exec(("rm -fr "+Dest_dir+"/"+infile+"*_Skim.root").c_str());
     
-    if (flag) {
+    // Warning of not found files
+        if (flag) {
         std::cout<<"\033[1;31m WARNING CREATED ERROR FILE \033[0m\n"<< endl;
     }
-    
-    if (!gSystem->AccessPathName((Dest_dir+"/tree_total_from_"+to_string(from)+"_to_"+to_string(to)+"_OLD.root").c_str(),kFileExists)){ //Returns true if not found
+    // Remember of previous files 
+    if (!gSystem->AccessPathName((Dest_dir+"/tree_"+source+"_from_"+to_string(from)+"_to_"+to_string(to)+"_OLD.root").c_str(),kFileExists)){ //Returns true if not found
         std::cout<<"\033[1;31m WARNING CREATED OLD FILE \033[0m\n"<< endl;
     }
     
--- AnalysisCode/skimData.sh
+++ AnalysisCode/skimData.sh
@@ -1,18 +1,38 @@
+export SOURCE_DIR="/gpfs/ddn/srm/cms/store/user"
+export source_dir_B_v2="$SOURCE_DIR/cvernier/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016B-23Sep2016-v2/170130_122257/0000"
+export source_dir_C_v1="$SOURCE_DIR/cvernier/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016C-23Sep2016-v1/170130_122516/0000"
+export source_dir_E_v1="$SOURCE_DIR/cvernier/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016E-23Sep2016-v1/170130_122704/0000"
+export source_dir_F_v1="$SOURCE_DIR/cvernier/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016F-23Sep2016-v1/170130_122818/0000"
+export source_dir_H_v1="$SOURCE_DIR/cvernier/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016H-PromptReco-v1/170130_123051/0000"
+export source_dir_H_v2="$SOURCE_DIR/cvernier/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016H-PromptReco-v2/170130_123206/0000"
+export source_dir_H_v2_1="$SOURCE_DIR/cvernier/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016H-PromptReco-v2/170130_123206/0001"
+export source_dir_H_v2_2="$SOURCE_DIR/cvernier/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016H-PromptReco-v2/170130_123206/0002"
+
+export source_dir_B_v3="$SOURCE_DIR/anmalara/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016B-23Sep2016-v3/170206_165040/0000"
+export source_dir_B_v3_1="$SOURCE_DIR/anmalara/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016B-23Sep2016-v3/170206_165040/0001"
+export source_dir_D_v1="$SOURCE_DIR/anmalara/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016D-23Sep2016-v1/170206_164949/0000"
+export source_dir_D_v1_1="$SOURCE_DIR/anmalara/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016D-23Sep2016-v1/170206_164949/0001"
+export source_dir_G_v1="$SOURCE_DIR/anmalara/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016G-23Sep2016-v1/170206_164902/0000"
+export source_dir_G_v1_1="$SOURCE_DIR/anmalara/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016G-23Sep2016-v1/170206_164902/0001"
+export source_dir_H_v3="$SOURCE_DIR/anmalara/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016H-PromptReco-v3/170206_164802/0000"
+
 export TEST_PATH="/scratch/malara/WorkingArea/IO_file"
-export source_dir="/gpfs/ddn/srm/cms/store/user/cvernier/VHBBHeppyV25/BTagCSV/VHBB_HEPPY_V25_BTagCSV__Run2016H-PromptReco-v2/170130_123206/0000/" 
-export dest_dir="$TEST_PATH/output_file" 
+export dest_dir="$TEST_PATH/output_file"
 
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",1,5)'
-
-root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",1,100)'
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",101,200)'
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",201,300)'
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",301,400)'
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",401,500)'
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",501,600)'
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",601,700)'
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",701,800)'
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",801,900)'
-#root -l -b -q 'Vskim.cc++("$source_dir","$dest_dir", "tree_",901,1000)'
+##root -l -b -q 'Vskim.cc++("$source_dir_B_v2","$dest_dir", "tree_",1,222)'
+root -l -b -q 'Vskim.cc++("$source_dir_C_v1","$dest_dir", "tree_",1,627)'
+#root -l -b -q 'Vskim.cc++("$source_dir_E_v1","$dest_dir", "tree_",1,907)'
+#root -l -b -q 'Vskim.cc++("$source_dir_F_v1","$dest_dir", "tree_",1,653)'
+##root -l -b -q 'Vskim.cc++("$source_dir_H_v1","$dest_dir", "tree_",1,191)'
+#root -l -b -q 'Vskim.cc++("$source_dir_H_v2","$dest_dir", "tree_",1,1000)'
+#root -l -b -q 'Vskim.cc++("$source_dir_H_v2_1","$dest_dir", "tree_",1000,2000)'
+#root -l -b -q 'Vskim.cc++("$source_dir_H_v2_2","$dest_dir", "tree_",2000,2018)'
+#root -l -b -q 'Vskim.cc++("$source_dir_B_v3","$dest_dir", "tree_",1,1000)'
+#root -l -b -q 'Vskim.cc++("$source_dir_B_v3_1","$dest_dir", "tree_",1000,1989)'
+#root -l -b -q 'Vskim.cc++("$source_dir_D_v1","$dest_dir", "tree_",1,1000)'
+#root -l -b -q 'Vskim.cc++("$source_dir_D_v1_1","$dest_dir", "tree_",1000,1010)'
+#root -l -b -q 'Vskim.cc++("$source_dir_G_v1","$dest_dir", "tree_",1,1000)'
+#root -l -b -q 'Vskim.cc++("$source_dir_G_v1_1","$dest_dir", "tree_",1000,1552)'
+##root -l -b -q 'Vskim.cc++("$source_dir_H_v3","$dest_dir", "tree_",1,46)'
 
 
